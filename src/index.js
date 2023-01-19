6932f1cb848e6c935289ee070e204d5551e4d956
Charles Tyra <charlestyra@MacBook-Pro.local>
2023-01-19 10:27:51 -0500

    got Google Map API to load, working through how to get Earth Engine on the screen

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 // Require client library and private key.
-const Auth = require('./scripts/auth.js');
-var ee = require('../node_modules/@google/earthengine');
+const ee = require('@google/earthengine');
+const express = require('express');
+const privateKey = require('../privatekey.json');
 
-// ee.data.authenticateViaPrivateKey(privateKey, runAnalysis, function (e) {
-//    console.error('Authentication error: ' + e);
-// });
+const port = process.env.PORT || 3000;
+
+// Define endpoint at /mapid.
+const app = express().get('/mapid', (_, response) => {
+   const srtm = ee.Image('CGIAR/SRTM90_V4');
+   const slope = ee.Terrain.slope(srtm);
+   slope.getMap({ min: 0, max: 60 }, ({ mapid }) => response.send(mapid));
+});
+
+console.log('Authenticating Earth Engine API using private key...');
+ee.data.authenticateViaPrivateKey(
+   privateKey,
+   () => {
+      console.log('Authentication successful.');
+      ee.initialize(
+         null, null,
+         () => {
+            console.log('Earth Engine client library initialized.');
+            app.listen(port);
+            console.log(`Listening on port ${port}`);
+         },
+         (err) => {
+            console.log(err);
+            console.log(
+               `Please make sure you have created a service account and have been approved.
+Visit https://developers.google.com/earth-engine/service_account#how-do-i-create-a-service-account to learn more.`);
+         });
+   },
+   (err) => {
+      console.log(err);
+   });
 
 document.addEventListener('DOMContentLoaded', () => {
-
+   // const root = document.getElementById('map')
 // Auth initialization -- focusing on html and css then coming back
    // Initialize client library.
    // console.log('before auth');
